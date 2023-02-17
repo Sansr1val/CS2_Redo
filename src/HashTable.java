11961aae779e3c76463359639589f2f6d4374001
DyamesGH <114891651+DyamesGH@users.noreply.github.com>
2023-02-17 19:20:30 +0800

    Nalamnan ng komento

--- src/HashTable.java
+++ src/HashTable.java
@@ -6,12 +6,22 @@ public class HashTable {
 			table[i] = new LinkedList();
 		}
 	}
-
+	
+	/*
+	 * add function accepts integer as key using the LinkedList class' append function.
+	 * The keys are then hashed by the HashFunction method. Uses separate chaining as 
+	 * solution for the collisions.  
+	 */
 	public void add(int key) {
 		int hash = hashFunction(key);
 		table[hash].append(new Node(key));
 	}
-
+	
+	/*
+	 * Shows the contents of the HashTable using the LinkedList class' displayInfo function.
+	 * Starts the display with an arrow "->" if the list is not empty,
+	 * otherwise it will display nothing.
+	 */
 	public String view() {	
 		String str = "";
 		for (int i = 0; i < table.length; i++) {
@@ -20,6 +30,11 @@ public class HashTable {
 		return str;
 	}
 
+	/*
+	 * Searches for a key in the Hash Table using the LinkedList class' find function.
+	 * THe hash is first computed by the hash function, the key is searched 
+	 * in the list found at the hash.
+	 */
 	public int search(int key) {
 		int hash = hashFunction(key);
 		if(table[hash].find(key)) {
@@ -28,7 +43,13 @@ public class HashTable {
 			return 0;
 		}
 	}
-
+	
+	/*
+	 * Deletes a key in the Hash Table using the LinkedList class' remove function.
+	 * The hash is computed by the hash function and the linked list at the hash is searched
+	 * for the key.
+	 * It returns the boolean value of the 
+	 */
 	public boolean delete(int key) {
 		int hash = hashFunction(key);
 		return table[hash].remove(key);
