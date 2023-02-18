3ebc81ece447dbb592103b7cb1d0969b558112d1
John Jomar <110670707+Sansr1val@users.noreply.github.com>
2023-02-18 08:43:45 +0800

    Comments

--- src/HashTable.java
+++ src/HashTable.java
@@ -6,11 +6,10 @@ public class HashTable {
 			table[i] = new LinkedList();
 		}
 	}
-	
 	/*
-	 * add function accepts integer as key using the LinkedList class' append function.
-	 * The keys are then hashed by the HashFunction method. Uses separate chaining as 
-	 * solution for the collisions.  
+	 * Adds a new integer in the list, accepts int key as its parameter.
+	 * The key is Hashed and inserted into list at the hash using the 
+	 * LinkedList class' append function.
 	 */
 	public void add(int key) {
 		int hash = hashFunction(key);
@@ -19,8 +18,9 @@ public class HashTable {
 	
 	/*
 	 * Shows the contents of the HashTable using the LinkedList class' displayInfo function.
-	 * Starts the display with an arrow "->" if the list is not empty.
-	 * Then returns str.
+	 * Starts the display with the index and an arrow "->" if the list is not empty,
+	 * otherwise only the index is shown.
+	 * Then returns String str.
 	 */
 	public String view() {	
 		String str = "";
@@ -33,24 +33,20 @@ public class HashTable {
 	/*
 	 * Searches for a key in the Hash Table using the LinkedList class' find function.
 	 * The hash is first computed by the hash function, then the key is searched 
-	 * in the list found at the hash.
-	 * If find function returns true, returns true, else if find function returns false, returns false.
+	 * in the list found at the hash using the LinkedList class' find function.
+	 * Returns true if the key is found in the list, else it returns false.
 	 */
 	public boolean search(int key) {
 		int hash = hashFunction(key);
-		if(table[hash].find(key)) {
-			return true;
-		}else {
-			return false;
-		}
+		return table[hash].find(key);
 	}
 	
 	/*
 	 * Deletes a key in the Hash Table using the LinkedList class' remove function.
 	 * The hash is computed by the hash function and the linked list at the hash is searched
 	 * for the key and deletes it.
-	 * It returns the boolean value of the boolean value that the remove function of the LinkedList 
-	 * class will return.
+	 * Returns true if the key is found and it will be deleted using remove() function,else
+	 * it return false if it does not exist.
 	 */
 	public boolean delete(int key) {
 		int hash = hashFunction(key);
